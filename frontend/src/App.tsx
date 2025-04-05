cce1b19023b7eb57421f126d2fc65cb769129824
Jerome Chua <jerome.chuame@gmail.com>
2025-04-05 16:01:44 +0800

    Create scaffolding. Add shadcn ui lib

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { PaginatedTable } from './PaginatedTable'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Jerome</h1>
+      <h1 className="text-3xl font-bold underline">Process First LLC</h1>
+      <PaginatedTable />
     </>
   )
 }
---
+++ frontend/src/PaginatedTable.tsx
@@ -0,0 +1,6 @@
+export const PaginatedTable = () => {
+
+    return (
+        <><h1>table</h1></>
+    )
+}
